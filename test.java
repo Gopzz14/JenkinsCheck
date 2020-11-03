5ce86ea609abe18a269d6262859e832f94de19d0
Gopzz14 <47500580+Gopzz14@users.noreply.github.com>
2020-11-03 15:48:40 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,14 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sq = "square";
+		String cir = "circle";
+
+		String obj = "square";
+
+		System.out.println("Jenkins Test");
+	}
+
+}
